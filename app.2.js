a15ad5bc15841cc2ef07157862fca91417a448ed
yangshangzhao <yangshangzhao@yangshangzhao.com>
2018-10-16 18:26:19 +0800

    add post req

---
+++ app.2.js
@@ -0,0 +1,31 @@
+// 导入koa, 在koa2中， 我们导入的是一个class，因此用大写的Koa表示：
+const Koa = require('koa');
+
+// 注意require('koa-router)返回的是函数:
+const router = require('koa-router')();
+
+const app = new Koa();
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.req.url}...`);
+    await next();
+});
+
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = `<h1>Index</h1>`;
+})
+
+// add router middleware:
+app.use(router.routes());
+
+
+//在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...');
--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 // 导入koa, 在koa2中， 我们导入的是一个class，因此用大写的Koa表示：
 const Koa = require('koa');
 
+// 导入koa-bodyparser:
+const bodyParser = require('koa-bodyparser');
 // 注意require('koa-router)返回的是函数:
 const router = require('koa-router')();
 
 const app = new Koa();
 
-// log request URL:
-app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.req.url}...`);
-    await next();
-});
-
-// add url-route:
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
+app.use(bodyParser());
 
 router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>`;
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"/></p>
+            <p>Password: <input name="password" type="password"/></p>
+            <p><input type="submit" value="Submit"/></p>
+        </form>`;
 })
+
+router.post('/signin', async (ctx, next) => {
+    var name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
 
 // add router middleware:
 app.use(router.routes());
