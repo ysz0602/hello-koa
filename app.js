40c7115c61a8b7f5bbc301fa02eea15279336746
yangshangzhao <yangshangzhao@yangshangzhao.com>
2018-10-16 17:54:49 +0800

    test url

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 // 导入koa, 在koa2中， 我们导入的是一个class，因此用大写的Koa表示：
 const Koa = require('koa');
 
-// 创建一个Koa对象表示web app本身:
+// 注意require('koa-router)返回的是函数:
+const router = require('koa-router')();
+
 const app = new Koa();
 
-//对于任何请求， app将调用该异步函数处理请求:
+// log request URL:
 app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.req.url}...`);
     await next();
-    // 设置response的Content-Type：
-    ctx.response.type = 'text/html';
-    // 设置response的内容:
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
+
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = `<h1>Index</h1>`;
+})
+
+// add router middleware:
+app.use(router.routes());
+
 
 //在端口3000监听:
 app.listen(3000);
